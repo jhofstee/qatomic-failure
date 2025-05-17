9ad56a2d3a1465799503f3c503cfab2fdd94f060
Jeroen Hofstee <jhofstee@victronenergy.com>
2025-05-17 16:25:53 +0200

    indent the reduced.cpp

--- reduced.cpp
+++ reduced.cpp
@@ -1,26 +1,32 @@
 struct QString;
+
 namespace std {
-template <typename _Tp> _Tp forward();
-template <typename _Tp> QString &&move(_Tp &&);
-template <typename _Tp, typename _Up> _Tp exchange(_Tp __obj, _Up) {
-  return __obj;
+  template <typename _Tp> _Tp forward();
+  template <typename _Tp> QString &&move(_Tp &&);
+  template <typename _Tp, typename _Up> _Tp exchange(_Tp __obj, _Up) {
+    return __obj;
+  }
+
+  struct atomic {
+    int _M_i;
+    void fetch_add(int __i) { __atomic_fetch_add(&_M_i, __i, int()); }
+  };
 }
-struct atomic {
-  int _M_i;
-  void fetch_add(int __i) { __atomic_fetch_add(&_M_i, __i, int()); }
-};
-} // namespace std
+
 struct {
   static void ref(std::atomic &_q_value) { _q_value.fetch_add(1); }
 } typedef Ops;
+
 struct QBasicAtomicInteger {
   std::atomic _q_value;
   void ref() { Ops::ref(_q_value); }
 };
+
 struct QTypedArrayData {
   QBasicAtomicInteger ref_;
   void ref() { ref_.ref(); }
 };
+
 template <class> struct QArrayDataPointer;
 template <class T> struct QMovableArrayOps : QArrayDataPointer<T> {
   struct Inserter {
@@ -32,9 +38,11 @@ template <class T> struct QMovableArrayOps : QArrayDataPointer<T> {
     Inserter(this, i, 1).insertOne(std::move(tmp));
   }
 };
+
 template <class> struct QArrayOpsSelector {
   typedef QMovableArrayOps<QString> Type;
 };
+
 template <class T> struct QCommonArrayOps : QArrayOpsSelector<T>::Type {};
 template <class> struct QArrayDataPointer {
   QArrayDataPointer();
@@ -49,17 +57,21 @@ template <class> struct QArrayDataPointer {
   int *ptr;
   int size;
 };
+
 struct QString {
   QString(char *);
   QArrayDataPointer<char16_t> d;
 } typedef parameter_type;
+
 QArrayDataPointer<int> d;
 template <typename... Args> void emplaceBack(Args &&...) {
   d->emplace(d->size, std::forward<Args>()...);
 }
+
 struct QList {
   void operator<<(parameter_type t) { emplaceBack(t); }
 };
+
 int main() {
   QString dirEntry("");
   QList() << dirEntry;
